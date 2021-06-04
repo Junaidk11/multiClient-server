b3c4eb0a074c5b5f68b42b077efcff504baed958
Junaidk11 <junaidkhan.tmk@hotmail.com>
2021-06-04 02:26:24 -0700

    Initial commit

---
+++ clientMain.c
@@ -0,0 +1,10 @@
+/*
+ *  Using select() to create a multiClient-Server communication.
+ *
+ */
+
+#include  <stdio.h>
+
+int main(int argc, char *argv[]){
+	return 0;
+}:
---
+++ serverMain.c
@@ -0,0 +1,11 @@
+ /*
+  *  Using Select() to setup a multiClient-Server Communication
+  */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+	return 0;
+
+}
